c0695ba8181bfb5b274935cba4708c7bec7dbf73
philippfhgr <philipp.horber@stud.fhgr.ch>
2024-05-08 18:13:14 +0200

    Belegung mit Farbe
    Auslastung_Prozent mit Farbe verknüpft. Hohe, mittlere und geringe Auslastung wird jetzt pro Punkt in Farbe dargestellt.
    Objekt ohne Daten im Code eingebaut und auf der Webseite ausgeblendet.
    PopUp als tatsächliches PopUp - Subject to change.

--- script.js
+++ script.js
@@ -1,5 +1,66 @@
-console.log('Hello, World');
-function myFunction() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
+// Zugriff auf die Popups nach dem Laden der Daten
+let popups = document.getElementsByClassName("popup");
+
+async function togglePopup(num) {
+  popups[num].classList.toggle("active");
+}
+
+async function holeDaten(url) {
+  try {
+    let data = await fetch(url);
+    return await data.json();
   }
+  catch (error) {
+    console.error(error);
+  }
+}
+
+async function getDataAndColorize() {
+  try {
+    let parkingData = await holeDaten("https://data.bs.ch/api/explore/v2.1/catalog/datasets/100088/records?limit=20");
+
+
+
+    for (let i = 0; i < parkingData.results.length; i++) {
+      let parkingLot = parkingData.results[i];
+      let occupancy = parkingLot.auslastung_prozent;
+      let knopfColor = getColorFromOccupancy(occupancy);
+
+      // Überprüfe, ob das .dot-Element existiert, bevor du darauf zugreifst
+      let dot = document.querySelectorAll(".dot")[i];
+      if (dot) {
+        dot.style.backgroundColor = knopfColor;
+        console.log("Farbe für Parkhaus " + parkingLot.name + ": " + knopfColor);
+      }
+
+      console.log(occupancy); // Ausgabe: Auslastung des Parkhauses
+    }
+
+    // Eventlistener für Popup-Buttons einrichten
+    let closeBtns = document.getElementsByClassName("close-btn");
+    let openBtns = document.getElementsByClassName("btn");
+
+    for (let i = 0; i < closeBtns.length; i++) {
+      closeBtns[i].addEventListener("click", function () { togglePopup(i) });
+      openBtns[i].addEventListener("click", function () { togglePopup(i) });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getDataAndColorize(); // Funktion aufrufen, um Daten abzurufen und die Knöpfe zu färben
+
+function getColorFromOccupancy(occupancy) {
+  if (occupancy >= 80) {
+    return "red"; // Rot bei hoher Auslastung
+  } else if (occupancy === null) {
+    return "grey "; // Gelb bei mittlerer Auslastung
+  } else if (occupancy >= 60) {
+    return "orange"; // Orange bei mittlerer Auslastung
+  } else {
+    return "green"; // Grün bei niedriger Auslastung
+  }
+}
+
+getColorFromOccupancy(); // Funktion aufrufen, um die Farbe des Knopfes zu bestimmen
